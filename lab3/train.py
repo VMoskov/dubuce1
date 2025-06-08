5207e2b627acea7d5f0804b4c688b5793737dd41
VMoskov <vedran.moskov@fer.hr>
2025-06-09 00:03:42 +0200

    updated trainer get device method

--- lab3/train.py
+++ lab3/train.py
@@ -97,13 +97,12 @@ class Trainer:
     
     @staticmethod
     def get_device():
-        # if torch.cuda.is_available():
-        #     return torch.device('cuda')
-        # elif torch.backends.mps.is_available():
-        #     return torch.device('mps')
-        # else:
-        #     return torch.device('cpu')
-        return torch.device('cpu')
+        if torch.cuda.is_available():
+            return torch.device('cuda')
+        elif torch.backends.mps.is_available():
+            return torch.device('mps')
+        else:
+            return torch.device('cpu')
         
     @staticmethod
     def build_model(embedding_layer, config):
--- lab4/train.py
+++ lab4/train.py
@@ -155,13 +155,12 @@ class Trainer:
 
     @staticmethod
     def get_device():
-        # if torch.cuda.is_available():
-        #     return torch.device('cuda')
-        # elif torch.backends.mps.is_available():
-        #     return torch.device('mps')
-        # else:
-        #     return torch.device('cpu')
-        return torch.device('cpu')
+        if torch.cuda.is_available():
+            return torch.device('cuda')
+        elif torch.backends.mps.is_available():
+            return torch.device('mps')
+        else:
+            return torch.device('cpu')
             
 
 if __name__ == '__main__':
